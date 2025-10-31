2f1d1aaa7da969ae356a815b7aa9b1ac7df48a69
noahbds <noah.eble@epitech.eu>
2025-10-31 15:37:30 +0100

    Initialize MiscCraft project with Fabric mod setup, Gradle configuration, GitHub Actions, and essential project structure.

---
+++ src/client/java/noahbds/miscm/MiscCraftClient.java
@@ -0,0 +1,10 @@
+package noahbds.miscm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MiscCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/noahbds/miscm/MiscCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package noahbds.miscm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MiscCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
